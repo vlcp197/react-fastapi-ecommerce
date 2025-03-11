5c34a480c6567bdff130081d72ad7873f9559f53
vlcp197 <vlcp197@gmail.com>
2025-03-11 20:48:29 -0300

    start to make layout component

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -4,7 +4,13 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 
 const Layout = () => {
-    
+    return (
+        <>
+            <Navbar/>
+            <Outlet/>
+            <Footer/>
+        </>
+    )
 }
 
 export default Layout;
